5090cec2912e37e21f9eadf8a252f956f3f4a6f3
emytek <66952283+emytek@users.noreply.github.com>
2022-12-13 03:14:34 +0100

    frontend-booking

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  //console.log(user)
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Jezron booking</span>
+        </Link>
+        {user ? user.details.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
